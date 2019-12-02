4898e2a43b6eb43cc60af305f25228932497cb28
Jerry Mozes <jerry@puppet.com>
2019-12-02 12:48:29 -0700

    Add an example website to IIS

--- modules/windows/manifests/init.pp
+++ modules/windows/manifests/init.pp
@@ -62,26 +62,66 @@ class windows {
     onlyif  => 'pending_dsc_reboot',
   }
 
-  # dsc_xwebsite {'Stop DefaultSite':
-  #   dsc_ensure       => 'present',
-  #   dsc_name         => 'Default Web Site',
-  #   dsc_state        => 'Stopped',
-  #   dsc_physicalpath => $defaultwebsitepath,
-  # }
+  #Add an example website to IIS
+  file { 'c:\\inetpub\\complete':
+    ensure => 'directory'
+  }
 
-  iis_site { 'basic':
-    ensure          => 'started',
-    physicalpath    => 'c:\\inetpub\\basic',
-    applicationpool => 'DefaultAppPool',
-    require         => [
-      File['basic'],
-      #Dsc_xwebsite['Stop DefaultSite'],
+  file { 'c:\\inetpub\\complete_vdir':
+    ensure => 'directory'
+  }
+
+  # Set Permissions
+
+  acl { 'c:\\inetpub\\complete':
+    permissions => [
+      {'identity' => 'IISCompleteGroup', 'rights' => ['read', 'execute']},
     ],
   }
 
-  file { 'basic':
-    ensure => 'directory',
-    path   => 'c:\\inetpub\\basic',
+  acl { 'c:\\inetpub\\complete_vdir':
+    permissions => [
+      {'identity' => 'IISCompleteGroup', 'rights' => ['read', 'execute']},
+    ],
+  }
+
+  # Configure IIS
+
+  iis_application_pool { 'complete_site_app_pool':
+    ensure                  => 'present',
+    state                   => 'started',
+    managed_pipeline_mode   => 'Integrated',
+    managed_runtime_version => 'v4.0',
+  }
+
+  #Application Pool No Managed Code .Net CLR Version set up
+  iis_application_pool {'test_app_pool':
+      ensure                    => 'present',
+      enable32_bit_app_on_win64 => true,
+      managed_runtime_version   => '',
+      managed_pipeline_mode     => 'Classic',
+      start_mode                => 'AlwaysRunning'
+    }
+
+  iis_site { 'complete':
+    ensure           => 'started',
+    physicalpath     => 'c:\\inetpub\\complete',
+    applicationpool  => 'complete_site_app_pool',
+    enabledprotocols => 'https',
+    bindings         => [
+      {
+        'bindinginformation' => '*:8080:',
+        'protocol'           => 'http',
+      },
+    ],
+    require => File['c:\\inetpub\\complete'],
+  }
+
+  iis_virtual_directory { 'vdir':
+    ensure       => 'present',
+    sitename     => 'complete',
+    physicalpath => 'c:\\inetpub\\complete_vdir',
+    require      => File['c:\\inetpub\\complete_vdir'],
   }
 
 }
