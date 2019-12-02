8c401dbc26efc2b21acbca01d3837239ea213d7f
Jerry Mozes <jerry@puppet.com>
2019-12-02 09:54:53 -0700

    add comments and disable ShutdownReasonOn

--- modules/windows/manifests/init.pp
+++ modules/windows/manifests/init.pp
@@ -28,16 +28,18 @@ class windows {
     ],
   }
 
+  # Code to enable/disable the IE Enhanced Security Configuration
   registry_value { 'HKLM\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
     ensure => present,
     type   => dword,
     data   => 0,
   }
 
+  # Entry to disable or enable the Shutdown Reason Box in Windows
   registry_value {'HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Reliability\ShutdownReasonOn':
     ensure => present,
     type   => dword,
-    data   => 1,
+    data   => 0,
   }
 
   registry_value {'HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Reliability\ShutdownReasonUI':
