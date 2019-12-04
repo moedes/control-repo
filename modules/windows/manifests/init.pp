3fbc7d56b6a123c417506b3221f4d4d2f4f2aba3
Jerry Mozes <jerry@puppet.com>
2019-12-04 15:05:53 -0600

    Disable IE ESC after completing testing

--- modules/windows/manifests/init.pp
+++ modules/windows/manifests/init.pp
@@ -33,7 +33,7 @@ class windows {
   registry_value { 'HKLM\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
     ensure => present,
     type   => dword,
-    data   => 1,
+    data   => 0,
   }
 
   # Entry to disable or enable the Shutdown Reason Box in Windows
