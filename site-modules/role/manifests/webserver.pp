68636d2aaeca3a14b445d6209f6a0534133a2cd5
Jerry Mozes <jerry@puppet.com>
2019-12-04 15:38:27 -0600

    syntax correction

--- site-modules/role/manifests/webserver.pp
+++ site-modules/role/manifests/webserver.pp
@@ -3,5 +3,5 @@ class role::webserver {
   #This role would be made of all the profiles that need to be included to make a webserver work
   #All roles should include the base profile
   include profile::windows_base
-  include profile::iis_complete_site.pp
+  include profile::iis_complete_site
 }
