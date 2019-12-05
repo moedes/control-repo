dad703bca313793513930b69e6c71d5200bcf708
Jerry Mozes <jerry@puppet.com>
2019-12-04 19:04:02 -0600

    Syntax change

--- site-modules/profile/manifests/windows_base.pp
+++ site-modules/profile/manifests/windows_base.pp
@@ -1,7 +1,7 @@
 #Windows base configuration profile
 class profile::windows_base {
-  include winapps
-  
+  include winapps::winapps
+
   # Code to enable/disable the IE Enhanced Security Configuration
   registry_value { 'HKLM\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
     ensure => present,
