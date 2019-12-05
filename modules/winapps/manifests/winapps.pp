0e7c868ca8095120b1f86e847fb95dc2b565b203
Jerry Mozes <jerry@puppet.com>
2019-12-04 18:56:45 -0600

    Added applications to winapps and removed

---
+++ modules/winapps/manifests/winapps.pp
@@ -0,0 +1,13 @@
+#Default Windows Apps
+class winapps {
+  include chocolatey
+
+  package {'7zip.install':
+    ensure   => installed,
+    provider => 'chocolatey',
+  }
+
+  reboot {'after':
+    subscribe => Package['7zip.install'],
+  }
+}
--- modules/windows/manifests/init.pp
+++ modules/windows/manifests/init.pp
@@ -4,7 +4,6 @@
 #
 # @example
 class windows {
-  $defaultwebsitepath = 'C:\inetpub\wwwroot'
 
   file { 'c:\users\services':
     ensure => 'directory',
@@ -19,90 +18,6 @@ class windows {
     ],
   }
 
-  # Add IIS to Windows Server using puppetlabs/dsc module
-  dsc_windowsfeature {'IIS':
-    dsc_ensure => 'present',
-    dsc_name   => 'Web-Server',
-  }
-
-  # Reboot when pending dsc reboot
-  reboot {'dsc_reboot':
-    message => 'DSC has requested a reboot',
-    when    => 'pending',
-    onlyif  => 'pending_dsc_reboot',
-  }
-
-  #Add an example website to IIS
-  file { 'c:\\inetpub\\complete':
-    ensure => 'directory'
-  }
-
-  file { 'c:\\inetpub\\complete_vdir':
-    ensure => 'directory'
-  }
-
-  # Set Permissions
-
-  acl { 'c:\\inetpub\\complete':
-    permissions => [
-      {'identity' => 'Services', 'rights' => ['read', 'execute']},
-    ],
-  }
-
-  acl { 'c:\\inetpub\\complete_vdir':
-    permissions => [
-      {'identity' => 'service_user', 'rights' => ['read', 'execute']},
-    ],
-  }
-
-  # Configure IIS
-
-  iis_application_pool { 'complete_site_app_pool':
-    ensure                  => 'present',
-    state                   => 'started',
-    managed_pipeline_mode   => 'Integrated',
-    managed_runtime_version => 'v4.0',
-  }
-
-  #Application Pool No Managed Code .Net CLR Version set up
-  iis_application_pool {'test_app_pool':
-      ensure                    => 'present',
-      enable32_bit_app_on_win64 => true,
-      managed_runtime_version   => '',
-      managed_pipeline_mode     => 'Classic',
-      start_mode                => 'AlwaysRunning'
-    }
-
-  iis_site { 'complete':
-    ensure           => 'started',
-    physicalpath     => 'c:\\inetpub\\complete',
-    applicationpool  => 'complete_site_app_pool',
-    enabledprotocols => 'https',
-    bindings         => [
-      {
-        'bindinginformation' => '*:8080:',
-        'protocol'           => 'http',
-      },
-    ],
-    require          => File['c:\\inetpub\\complete'],
-  }
-
-  iis_virtual_directory { 'vdir':
-    ensure       => 'present',
-    sitename     => 'complete',
-    physicalpath => 'c:\\inetpub\\complete_vdir',
-    require      => File['c:\\inetpub\\complete_vdir'],
-  }
-  include chocolatey
-
-  package {'7zip.install':
-    ensure   => installed,
-    provider => 'chocolatey',
-  }
-
-  reboot {'after':
-    subscribe => Package['7zip.install'],
-  }
 }
 
 
