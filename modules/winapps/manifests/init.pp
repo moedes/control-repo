ae7b5e9f0846ce0095ca07e3906a950f645b614c
Jerry Mozes <jerry@puppet.com>
2019-12-12 16:17:38 -0700

    commented out 7zip.install subscription

--- modules/winapps/manifests/init.pp
+++ modules/winapps/manifests/init.pp
@@ -11,7 +11,7 @@ class winapps {
     provider => $provider,
   }
 
-  reboot {'after':
-    subscribe => Package['7zip.install'],
-  }
+  #reboot {'after':
+  #  subscribe => Package['7zip.install'],
+  #}
 }
