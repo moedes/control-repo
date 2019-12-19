af13505004bc382924bd6ad27f02b03c6e349a35
Jerry Mozes <jerry@puppet.com>
2019-12-19 13:01:01 -0700

    Change os_patching patch_window

--- modules/patch_mgmt/init.pp
+++
@@ -1,4 +0,0 @@
-include os_patching
-class {'os_patching':
-  patch_window => 'Week1',
-}
--- site-modules/profile/manifests/windows_base.pp
+++ site-modules/profile/manifests/windows_base.pp
@@ -2,7 +2,11 @@
 class profile::windows_base {
 
   include winapps
-  include patch_mgmt
+  include os_patching
+
+  class {'os_patching':
+    patch_window => 'Week1'
+  }
 
   # Code to enable/disable the IE Enhanced Security Configuration
   registry_value { 'HKLM\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
